5fb7d7422b2b0f87ea00398e8f373f37984d7697
Curiousity9 <1124393459@qq.com>
2020-04-13 15:04:43 +0800

    testspringmvc

---
+++ src/main/test/testGit.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * @author xiangwehao
+ * create 2020/4/13
+ */
+public class testGit {
+    @Test
+    public void test(){
+        System.out.println("mm");
+    }
+}
